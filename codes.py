70be9ded418095a01487db66ffbefe0cda77da9d
USER <user@USERui-MacBookPro.local>
2026-01-15 12:24:44 +0500

    training examples

---
+++ codes.py
@@ -0,0 +1,13 @@
+# sonlar = [1, 5, 12, 8, 20, 7,14]
+# katta_sonlar = [ ]
+# for son in sonlar:
+#     if son > 10:
+#         katta_sonlar.append(son)
+# print(katta_sonlar)
+
+ismlar = ["Ali", "Vali", "Adiba", "Gulinur"]
+ismlar_new = [ ]
+for ism in ismlar:
+    if ism.startswith("A"):
+        ismlar_new.append(ism)
+print(ismlar_new)
